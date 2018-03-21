09216d5f566ba31957b40386a70fc994a53c0ce7
kawboyer <kawboyer@gmail.com>
2018-03-20 22:36:09 -0400

    initial file structure with missing parts

---
+++ liri.js
@@ -0,0 +1,20 @@
+
+require("dotenv").config();
+
+// Add the code required to import the `keys.js` file and store it in a variable.
+
+
+
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// 10. Make it so liri.js can take in one of the following commands:
+
+    //* `my-tweets`
+
+    //* `spotify-this-song`
+
+    //* `movie-this`
+
+    //* `do-what-it-says`
---
+++ spotifykey.js
@@ -0,0 +1,2 @@
+// Spotify Client ID: 19ff0cbb2cd7409cafa47d220d0f3685
+// Spotify Client Secret: 380967dc0b344403ace81f673d9e9f71
